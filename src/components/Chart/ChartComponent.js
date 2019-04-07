551bc292f5319693c07d1843f16c3ac676096d40
James Tucker <jamesctucker@protonmail.com>
2019-04-07 13:52:54 -0500

    set up two line series for chart

--- src/components/Chart/ChartComponent.js
+++ src/components/Chart/ChartComponent.js
@@ -4,6 +4,8 @@ import { XYPlot, XAxis, YAxis, HorizontalGridLines, LineSeries } from 'react-vis
 import Datetime from 'react-datetime'
 import './Chart.css';
 
+
+
 class ChartComponent extends Component {
     state = {
         readings: [],
@@ -21,6 +23,8 @@ class ChartComponent extends Component {
 
 
     render() {
+        const MSEC_DAILY = 86400000;
+        const timestamp = new Date().getTime();
 
         return (
             <div>
@@ -32,15 +36,28 @@ class ChartComponent extends Component {
                     <Datetime />
                 </div>
                 <XYPlot
+                    xType="time"
                     width={300}
                     height={300}>
                     <HorizontalGridLines />
                     <LineSeries
+                        color="red"
                         data={[
-                            { x: 1, y: 13 },
-                            { x: 2, y: 5 },
-                            { x: 3, y: 15 }
-                        ]} />
+                            { x: timestamp + MSEC_DAILY, y: 3 },
+                            { x: timestamp + MSEC_DAILY * 2, y: 5 },
+                            { x: timestamp + MSEC_DAILY * 3, y: 15 },
+                            { x: timestamp + MSEC_DAILY * 4, y: 12 }
+                        ]}
+                    />
+                    <LineSeries data={null} />
+                    <LineSeries
+                        data={[
+                            { x: timestamp + MSEC_DAILY, y: 10 },
+                            { x: timestamp + MSEC_DAILY * 2, y: 4 },
+                            { x: timestamp + MSEC_DAILY * 3, y: 2 },
+                            { x: timestamp + MSEC_DAILY * 4, y: 15 }
+                        ]}
+                    />
                     <XAxis />
                     <YAxis />
                 </XYPlot>

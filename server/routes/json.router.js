0c5a576fb9391916252e2612433c007b6add6789
James Tucker <jamesctucker@protonmail.com>
2019-04-06 21:51:21 -0500

    decided to use just node to read csv and create json file

---
+++ server/routes/json.router.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+//require the csvtojson converter class
+const csvFilePath = 'server/routes/data.csv'
+const csv = require('csvtojson')
+
+csv()
+    .fromFile(csvFilePath)
+    .then((jsonObj) => {
+        console.log(jsonObj);
+        fs.writeFile('data/data.json', JSON.stringify(jsonObj), function (err) {
+            if (err) {
+                return console.log(err);
+            }
+            console.log("the file was saved!");
+        });
+    })
+
+// const jsonArray = await csv().fromFile(csvFilePath);
+
+
+
+
+
+
+module.exports = router;

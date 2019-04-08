21f8dfdda779d4ff4b554c0ffd8e86a2e586a22e
James Tucker <jamesctucker@protonmail.com>
2019-04-08 15:15:59 -0500

    added csv reader into this project

--- server/server.js
+++ server/server.js
@@ -1,6 +1,36 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+// these are required for csv reader/converter
+const fs = require('fs');
+//require the csvtojson converter class
+const csvFilePath = 'data/data.csv'
+const csv = require('csvtojson')
+
+
+// this function converts csv file to json, and then writes the json to an existing
+// empty json file
+csv({
+    noheader: false,
+    headers: ['timestamp', 'sensor', 'people'],
+    colParser: {
+        'people': 'number',
+    },
+    checkType: true
+})
+    .fromFile(csvFilePath)
+    .then((jsonObj) => {
+        console.log(jsonObj);
+        fs.writeFile('data/data.json', JSON.stringify(jsonObj), function (err) {
+            if (err) {
+                return console.log(err);
+            }
+            console.log("the file was saved!");
+        });
+    })
+
+// const jsonArray = await csv().fromFile(csvFilePath);
+
 
 const port = process.env.PORT || 5001;
 

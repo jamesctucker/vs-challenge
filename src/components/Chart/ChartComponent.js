ce9be2dc1e469de43414518518cbff6a2f1d0a1f
James Tucker <jamesctucker@protonmail.com>
2019-04-08 14:44:53 -0500

    added styling with material ui

--- src/components/Chart/ChartComponent.js
+++ src/components/Chart/ChartComponent.js
@@ -5,19 +5,27 @@ import Datetime from 'react-datetime';
 import moment from 'moment';
 import './Chart.css';
 
+import Card from '@material-ui/core/Card';
+import Grid from '@material-ui/core/Grid';
+import Divider from '@material-ui/core/Divider';
+import StopIcon from '@material-ui/icons/Stop';
+
+// this component fetches api data and builds out the timeseries chart
 class ChartComponent extends Component {
     constructor(props) {
         super(props);
         this.state = {
             readings: [],
-            start_date: "2019-01-01T05:00:00.000Z",
-            end_date: "2019-04-01T05:00:00.000Z",
+            start_date: '2019-01-01T05:00:00.000Z',
+            end_date: '2019-04-01T05:00:00.000Z',
             errors: null,
         }
     }
+
+    // this pulls in json array from api
     componentDidMount() {
         axios
-            .get("https://api.jsonbin.io/b/5caac4fe85438b0272f11548")
+            .get('https://api.jsonbin.io/b/5caac4fe85438b0272f11548')
             .then(response => {
                 this.setState({
                     readings: response.data,
@@ -28,6 +36,7 @@ class ChartComponent extends Component {
             .catch(error => this.setState({ error }));
     }
 
+    // this transforms sensor names into objects that can be sorted/filtered
     filterUniqueSensors = (sensors) => {
         let uniqueObject = {};
         for (let name of sensors) {
@@ -39,6 +48,8 @@ class ChartComponent extends Component {
         }
         return uniqueArray;
     }
+
+    // this takes the sensors and groups them by name, e.g. Sensor One, Sensor Two, etc.
     buildData = () => {
         let sensorNames = this.filterUniqueSensors(this.state.readings);
         let groupedSensors = []
@@ -60,11 +71,14 @@ class ChartComponent extends Component {
 
         return currentData;
     }
+
+    // a series of functions that filter sensors for chart based on name and timestamp
     sensorOne = () => {
         let dataSet = []
         //  console.log(this.buildData().length);
         this.buildData().filter(z => {
-            if (z.sensor === "Sensor 1" && z.timestamp >= this.state.start_date && z.timestamp <= this.state.end_date) {
+            // timestamp is sourced from state. state can be updated by user selecting timestamp from datetime calendar dropdown
+            if (z.sensor === 'Sensor 1' && z.timestamp >= this.state.start_date && z.timestamp <= this.state.end_date) {
                 dataSet.push({ x: new Date(z.timestamp).getTime() + 86400000, y: z.people })
             }
         })
@@ -73,7 +87,7 @@ class ChartComponent extends Component {
     sensorTwo = () => {
         let dataSet = []
         this.buildData().filter(z => {
-            if (z.sensor === "Sensor 2" && z.timestamp >= this.state.start_date && z.timestamp <= this.state.end_date) {
+            if (z.sensor === 'Sensor 2' && z.timestamp >= this.state.start_date && z.timestamp <= this.state.end_date) {
                 dataSet.push({ x: new Date(z.timestamp).getTime() + 86400000, y: z.people })
             }
         })
@@ -82,7 +96,7 @@ class ChartComponent extends Component {
     sensorThree = () => {
         let dataSet = []
         this.buildData().filter(z => {
-            if (z.sensor === "Sensor 3" && z.timestamp >= this.state.start_date && z.timestamp <= this.state.end_date) {
+            if (z.sensor === 'Sensor 3' && z.timestamp >= this.state.start_date && z.timestamp <= this.state.end_date) {
                 dataSet.push({ x: new Date(z.timestamp).getTime() + 86400000, y: z.people })
             }
         })
@@ -93,52 +107,64 @@ class ChartComponent extends Component {
 
 
     render() {
-
+        // this prevents users from selecting specific dates
+        let valid = function (moment) {
+            return moment.isBetween('2018-12-31', '2019-04-01')
+        }
         return (
             <div>
                 {/* {JSON.stringify(this.state.start_date)} */}
                 <h1>Chart Component</h1>
-                <div id="start-date">
+                {/* inputs for selecting date/time to sort chart data by */}
+                <div id='start-date'>
                     <Datetime
-                        onChange={(e) => { this.setState({ start_date: moment(e).toJSON() }) }}
+                        isValidDate={valid} onChange={(e) => { this.setState({ start_date: moment(e).toJSON() }) }}
                     />
                 </div>
-                <div id="end-date">
+                <div id='end-date'>
                     <Datetime
-                        onChange={(e) => { this.setState({ end_date: moment(e).toJSON() }) }}
+                        isValidDate={valid} onChange={(e) => { this.setState({ end_date: moment(e).toJSON() }) }}
                     />
                 </div>
-                <div id="end-date">
-                    <button>Filter</button>
-                </div>
-                <h3>Key</h3>
-                <ul>
-                    <li>Red: Sensor 1</li>
-                    <li>Blue: Sensor 2</li>
-                    <li>Green: Sensor 3</li>
-                </ul>
-                <XYPlot
-                    id="chart"
-                    xType="time"
-                    width={1200}
-                    height={400}>
-                    <HorizontalGridLines />
-                    <LineSeries
-                        data={this.sensorOne()}
-                        style={{ stroke: 'red', strokeWidth: 3 }}
-                    />
-                    <LineSeries
-                        data={this.sensorTwo()}
-                        style={{ stroke: 'blue', strokeWidth: 3 }}
-                    />
-                    <LineSeries
-                        data={this.sensorThree()}
-                        style={{ stroke: 'green', strokeWidth: 3 }}
-                    />
-                    <XAxis title="Timestamp" />
-                    <YAxis title="People Detected" />
-                </XYPlot>
-
+                <Grid container spacing={24}>
+                    <Grid item xl>
+                        <Card id='key-card'>
+                            <h3>Key</h3>
+                            <Divider />
+                            <StopIcon id='red-icon' /><p>Sensor 1</p>
+                            <Divider />
+                            <StopIcon id='blue-icon' /><p>Sensor 2</p>
+                            <Divider />
+                            <StopIcon id='green-icon' /><p>Sensor 3</p>
+                        </Card>
+                    </Grid>
+                    {/* react-vis chart component */}
+                    <Grid item xl>
+                        <Card id='chart-card'>
+                            <XYPlot
+                                id='chart'
+                                xType='time'
+                                width={1200}
+                                height={400}>
+                                <HorizontalGridLines />
+                                <LineSeries
+                                    data={this.sensorOne()}
+                                    style={{ stroke: 'red', strokeWidth: 3 }}
+                                />
+                                <LineSeries
+                                    data={this.sensorTwo()}
+                                    style={{ stroke: 'blue', strokeWidth: 3 }}
+                                />
+                                <LineSeries
+                                    data={this.sensorThree()}
+                                    style={{ stroke: 'green', strokeWidth: 3 }}
+                                />
+                                <XAxis title='Date/Time' />
+                                <YAxis title='People Detected' />
+                            </XYPlot>
+                        </Card>
+                    </Grid>
+                </Grid>
             </div>
         )
     }
